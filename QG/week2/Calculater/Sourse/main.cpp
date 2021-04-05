3069aaa727a80d519b2f94dc51a7b784ee4db267
lee-roger <81203726+lee-roger@users.noreply.github.com>
2021-04-05 22:40:31 +0800

    Add files via upload

---
+++ QG/week2/Calculater/Sourse/main.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "calculator.h"
+#include<stdlib.h>
+#include<conio.h>
+#include<string.h>
+#define MAXSIZE 100
+int main()
+{
+	char s1[MAXSIZE];			//用于存储前缀表达式 
+	char s2[MAXSIZE];			//用于存储转换后的表达式 
+	printf("请输入表达式:");
+	scanf("%s",s1);
+	
+	Tran(s1,s2);				//处理字符串，并转化为后缀表达式,存放在s2中 
+	printf("%s",s2);
+		
+	return 0;
+}
